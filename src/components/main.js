1537dba4d3f3a5b50aaba1abdb1840fb7627b745
Jonathan <jaml@Jonathans-MacBook-Pro.local>
2017-10-01 19:04:13 -0600

    Initial commit

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const styles = {
+  container: {
+
+  }
+}
+
+const Main = (props) => {
+  return (
+    <div style={styles.container}>
+      This is &lt;Main /&gt; component.
+    </div>
+  );
+};
+
+export default Main;
